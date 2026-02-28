2138dece52999f61591c6377b27c1ed252b4aa3e
jlswbs <jlswbs@users.noreply.github.com>
2026-02-28 19:48:18 +0100

    Add files via upload

---
+++ FFT_SpectralAlienoid/FFT_SpectralAlienoid.ino
@@ -0,0 +1,100 @@
+// Spectral alienoid FFT hardware //
+
+#include "timer.h"
+#include "fft.h"
+
+#define FFT_N           512
+#define SAMPLE_RATE     44100
+#define TIMER_RATE      1000000000/SAMPLE_RATE
+#define PWM_RATE        1000000
+#define AUDIO_PIN_L     6
+#define AUDIO_PIN_R     8
+
+static int16_t audio_out[FFT_N];
+volatile int sample_ptr = 0;
+
+fft_data_t fft_in_data[FFT_N] __attribute__((aligned(64)));
+fft_data_t fft_out_data[FFT_N] __attribute__((aligned(64)));
+
+int audio_callback(void *ctx) {
+
+    int16_t s = audio_out[sample_ptr];
+    float duty = (float)(s + 32768) / 65536.0f;
+
+    pwm_set_frequency(PWM_DEVICE_1, PWM_CHANNEL_0, PWM_RATE, (double)duty);
+    pwm_set_frequency(PWM_DEVICE_1, PWM_CHANNEL_1, PWM_RATE, (double)duty);
+
+    sample_ptr++;
+    if (sample_ptr >= FFT_N) sample_ptr = 0;
+    return 0;
+
+}
+
+void setup() {
+
+    srand(read_cycle());
+
+    sysctl_clock_enable(SYSCTL_CLOCK_FFT);
+    sysctl_reset(SYSCTL_RESET_FFT);
+    
+    dmac_init();
+
+    fpioa_set_function(AUDIO_PIN_L, FUNC_TIMER1_TOGGLE1);
+    fpioa_set_function(AUDIO_PIN_R, FUNC_TIMER1_TOGGLE2);
+    
+    plic_init();
+    sysctl_enable_irq();
+    
+    timer_init(TIMER_DEVICE_0);
+    timer_set_interval(TIMER_DEVICE_0, TIMER_CHANNEL_0, TIMER_RATE);
+    timer_irq_register(TIMER_DEVICE_0, TIMER_CHANNEL_0, 0, 1, audio_callback, NULL);
+    timer_set_enable(TIMER_DEVICE_0, TIMER_CHANNEL_0, 1);
+    
+    pwm_init(PWM_DEVICE_1);
+    pwm_set_enable(PWM_DEVICE_1, PWM_CHANNEL_0, 1);
+    pwm_set_enable(PWM_DEVICE_1, PWM_CHANNEL_1, 1);
+
+}
+
+void loop() {
+
+    static uint32_t flux = 0;
+    static int16_t feedback_register = 0;
+    flux++;
+
+    feedback_register ^= audio_out[rand() % FFT_N];
+
+    for (int i = 0; i < FFT_N / 2; i++) {
+
+        if ((flux % 4) == 0) {
+            fft_in_data[i].R1 ^= feedback_register; 
+            fft_in_data[i].I1 &= (flux << 2);
+        }
+
+        if (i == (flux % (FFT_N / 2))) {
+            fft_in_data[i].R1 += 20000; 
+            fft_in_data[i].I2 -= 20000;
+        }
+        
+        fft_in_data[i].R1 >>= 1;
+        fft_in_data[i].I1 >>= 1;
+
+    }
+
+    fft_complex_uint16_dma(DMAC_CHANNEL1, DMAC_CHANNEL2, 0, FFT_DIR_BACKWARD, (uint64_t *)fft_in_data, FFT_N, (uint64_t *)fft_out_data);
+
+    for (int i = 0; i < FFT_N / 2; i++) {
+
+        int16_t out_r = fft_out_data[i].R1;
+        int16_t noise_gate = (rand() % 100 > 95) ? 0 : out_r;
+        audio_out[2 * i]     = noise_gate ^ (flux & 0xFF); 
+        audio_out[2 * i + 1] = (fft_out_data[i].R2 >> 2) ^ feedback_register;
+
+    }
+
+    int drift = 5 + (flux % 12);
+    if (flux % 200 == 0) drift = 150;
+    
+    delay(drift);
+
+}
