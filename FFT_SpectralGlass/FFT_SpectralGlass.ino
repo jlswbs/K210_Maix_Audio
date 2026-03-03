48c8d1329f780ffeedda261545fa001227a77939
jlswbs <jlswbs@users.noreply.github.com>
2026-03-03 10:08:07 +0100

    Add files via upload

---
+++ FFT_SpectralGlass/FFT_SpectralGlass.ino
@@ -0,0 +1,98 @@
+// Spectral glass FFT hardware //
+
+#include "timer.h"
+#include "fft.h"
+
+#define FFT_N           512
+#define SAMPLE_RATE     44100
+#define TIMER_RATE      1000000000/SAMPLE_RATE
+#define PWM_RATE        1000000
+#define AUDIO_PIN_L     6
+#define AUDIO_PIN_R     8
+
+static int16_t audio_out[FFT_N];
+volatile int sample_ptr = 0;
+
+fft_data_t fft_in_data[FFT_N] __attribute__((aligned(64)));
+fft_data_t fft_out_data[FFT_N] __attribute__((aligned(64)));
+
+int audio_callback(void *ctx) {
+
+    int16_t s = audio_out[sample_ptr];
+    float duty = (float)(s + 32768) / 65536.0f;
+
+    pwm_set_frequency(PWM_DEVICE_1, PWM_CHANNEL_0, PWM_RATE, (double)duty);
+    pwm_set_frequency(PWM_DEVICE_1, PWM_CHANNEL_1, PWM_RATE, (double)duty);
+
+    sample_ptr++;
+    if (sample_ptr >= FFT_N) sample_ptr = 0;
+    return 0;
+
+}
+
+void setup() {
+
+    srand(read_cycle());
+
+    sysctl_clock_enable(SYSCTL_CLOCK_FFT);
+    sysctl_reset(SYSCTL_RESET_FFT);
+    
+    dmac_init();
+
+    fpioa_set_function(AUDIO_PIN_L, FUNC_TIMER1_TOGGLE1);
+    fpioa_set_function(AUDIO_PIN_R, FUNC_TIMER1_TOGGLE2);
+    
+    plic_init();
+    sysctl_enable_irq();
+    
+    timer_init(TIMER_DEVICE_0);
+    timer_set_interval(TIMER_DEVICE_0, TIMER_CHANNEL_0, TIMER_RATE);
+    timer_irq_register(TIMER_DEVICE_0, TIMER_CHANNEL_0, 0, 1, audio_callback, NULL);
+    timer_set_enable(TIMER_DEVICE_0, TIMER_CHANNEL_0, 1);
+    
+    pwm_init(PWM_DEVICE_1);
+    pwm_set_enable(PWM_DEVICE_1, PWM_CHANNEL_0, 1);
+    pwm_set_enable(PWM_DEVICE_1, PWM_CHANNEL_1, 1);
+
+}
+
+void loop() {
+
+    for (int i = 0; i < FFT_N / 2; i++) {
+
+        float decay = (i % 16 == 0) ? 0.992f : 0.75f;
+        
+        fft_in_data[i].R1 *= decay;
+        fft_in_data[i].I1 *= decay;
+        fft_in_data[i].R2 *= decay;
+        fft_in_data[i].I2 *= decay;
+
+    }
+
+    if (rand() % 10 > 6) {
+
+        int high_bin = (rand() % 180) + 50; 
+        int16_t amp = rand() % 8000;
+        
+        if (high_bin % 2 == 0) {
+            fft_in_data[high_bin / 2].R1 += amp;
+            fft_in_data[high_bin / 2].I1 += (rand() % 1000);
+        } else {
+            fft_in_data[high_bin / 2].R2 += amp;
+            fft_in_data[high_bin / 2].I2 += (rand() % 1000);
+        }
+
+    }
+
+    fft_complex_uint16_dma(DMAC_CHANNEL1, DMAC_CHANNEL2, 0, FFT_DIR_BACKWARD, (uint64_t *)fft_in_data, FFT_N, (uint64_t *)fft_out_data);
+
+    for (int i = 0; i < FFT_N / 2; i++) {
+
+        audio_out[2 * i]     = fft_out_data[i].R1;
+        audio_out[2 * i + 1] = fft_out_data[i].R2;
+
+    }
+
+    delay(3);
+
+}
